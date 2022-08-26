605d7f40e4f7b879c746a6f346cb50bd82cffa59
Rei Sikk <reix0010@stud.kea.dk>
2022-08-26 15:52:13 +0200

    equations work on operator click

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ let resultList = document.querySelector("#results");
 let lastResult = resultList.lastElementChild;
 let value = operator.value;
 const calcBtn = document.querySelector("#calculate");
-////Doing the calculation
+let newSelector;
 
-document.querySelector("#calculate").addEventListener("click", calcFunc);
+////Read the operator
+readOperator();
+function readOperator() {
+  operator.addEventListener("change", (event) => {
+    newSelector = event.target.value;
+  });
+  buttonClicked();
+  console.log(newSelector);
+}
+
+function buttonClicked() {
+  document.querySelector("#calculate").addEventListener("click", calcFunc);
+}
 
 function readFirstNumber() {
   firstNumber = parseFloat(document.querySelector("#firstnumber").value);
@@ -19,34 +31,29 @@ function readSecondNumber() {
   secondNumber = parseFloat(document.querySelector("#secondnumber").value);
   console.log("second number is:" + secondNumber);
 }
-function readOperator() {
-  let text = operator.options[operator.selectedIndex].text;
-  let value = operator.value;
-  console.log(value);
-}
 
 //Doing the (optional) rounding
 
 //Writing the result
 function calcFunc() {
-  readOperator();
+  /* readOperator(); */
   console.log("calcFunc called");
-  if (value === "add") {
+  if (newSelector === "add") {
     let result = parseFloat(firstNumber.value) + parseFloat(secondNumber.value);
     lastResult.textContent = result;
     console.log(result);
-  } else if (value === "sub") {
-    console.log(result);
-    result = parseFloat(firstNumber.value) - parseFloat(secondNumber.value);
+  } else if (newSelector === "sub") {
+    let result = parseFloat(firstNumber.value) - parseFloat(secondNumber.value);
     lastResult.textContent = result;
-  } else if (value === "div") {
     console.log(result);
+  } else if (newSelector === "div") {
+    let result = parseFloat(firstNumber.value) / parseFloat(secondNumber.value);
     lastResult.textContent = result;
-    result = parseFloat(firstNumber.value) / parseFloat(secondNumber.value);
-  } else if (value === "mul") {
     console.log(result);
+  } else if (newSelector === "mul") {
+    let result = parseFloat(firstNumber.value) * parseFloat(secondNumber.value);
     lastResult.textContent = result;
-    result = parseFloat(firstNumber.value) * parseFloat(secondNumber.value);
+    console.log(result);
   }
 }
 
