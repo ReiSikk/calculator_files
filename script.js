d20794942c838d1d0dd294b8eb0e070e00bb8c98
Rei Sikk <reix0010@stud.kea.dk>
2022-08-28 13:31:57 +0200

    finished calc and cleaned up code

--- script.js
+++ script.js
@@ -1,78 +1,80 @@
-/////Reading the numbers and operator
-let firstNumber = document.querySelector("#firstnumber");
-let secondNumber = document.querySelector("#secondnumber");
-let operator = document.querySelector("#operator");
-let selector = operator.options[operator.selectedIndex].text;
+"use strict";
+
+window.addEventListener("load", init);
+
 let resultList = document.querySelector("#results");
 let lastResult = resultList.lastElementChild;
 let value = operator.value;
 const calcBtn = document.querySelector("#calculate");
-let newSelector;
-let callCount = 0;
+let resultRounded;
+let result;
 
-////Read the operator
-readOperator();
-function readOperator() {
-  operator.addEventListener("change", (event) => {
-    newSelector = event.target.value;
-  });
-  buttonClicked();
-  console.log(newSelector);
+function init() {
+  calcBtn.addEventListener("click", calcFunc);
+  document.querySelector("#clear").addEventListener("click", clearAll);
 }
 
-function buttonClicked() {
-  document.querySelector("#calculate").addEventListener("click", calcFunc);
-}
-
-function readFirstNumber() {
-  firstNumber = parseFloat(document.querySelector("#firstnumber").value);
-  console.log("second number is:" + firstNumber);
-}
-function readSecondNumber() {
-  secondNumber = parseFloat(document.querySelector("#secondnumber").value);
-  console.log("second number is:" + secondNumber);
-}
-
-//Doing the (optional) rounding
-
-//Writing the result
+//calculating the equation
 function calcFunc() {
-  /* readOperator(); */
-  console.log("calcFunc called");
-  if (newSelector === "add") {
-    let result = parseFloat(firstNumber.value) + parseFloat(secondNumber.value);
-    lastResult.textContent = result;
-    console.log(result);
-  } else if (newSelector === "sub") {
-    let result = parseFloat(firstNumber.value) - parseFloat(secondNumber.value);
-    lastResult.textContent = result;
-    console.log(result);
-  } else if (newSelector === "div") {
-    let result = parseFloat(firstNumber.value) / parseFloat(secondNumber.value);
-    lastResult.textContent = result;
-    console.log(result);
-  } else if (newSelector === "mul") {
-    let result = parseFloat(firstNumber.value) * parseFloat(secondNumber.value);
-    lastResult.textContent = result;
+  //read the operator and input string values
+  const operator = document.querySelector("#operator").value;
+  const firstString = document.querySelector("#firstnumber").value;
+  const secondString = document.querySelector("#secondnumber").value;
+  //convert strings to numbers
+  const firstNumber = Number.parseFloat(firstString);
+  const secondNumber = Number.parseFloat(secondString);
+
+  if (operator === "add") {
+    result = firstNumber + secondNumber;
+  }
+  if (operator === "sub") {
+    result = firstNumber - secondNumber;
+  }
+  if (operator === "div") {
+    result = firstNumber / secondNumber;
+  }
+  if (operator === "mul") {
+    result = firstNumber * secondNumber;
     console.log(result);
   }
-  /*   callCount += 1;
-  console.log(callCount);
-  moveResultUp(); */
+  //give the first number, the latest result value
+  document.querySelector("#firstnumber").value = result;
+  //reading if the rounding checkbox is checked
+  const rounding = document.querySelector("#doround").checked;
+  //doing the rounding
+  if (rounding) {
+    const decimals = document.querySelector("#decimals").value;
+    resultRounded = result.toFixed(decimals);
+    document.querySelector("#firstnumber").value = resultRounded;
+    appendRounded();
+  } else {
+    appendNumber();
+  }
 }
-/* function updateDisplay() {
-  let lastResult = resultList.firstElementChild;
-}
-function moveResultUp() {
-  lastResult.addEventListener("change", updateDisplay);
-} */
 
+////Appending the results
+function appendNumber() {
+  let li = document.querySelector("li");
+  li.textContent = result;
+  resultList.append(li);
+  resultList.scrollTo(0, 1000000);
+}
+function appendRounded() {
+  let li = document.querySelector("li");
+  li.textContent = resultRounded;
+  resultList.append(li);
+  resultList.scrollTo(0, 1000000);
+}
 //Clearing all the results
-document.querySelector("#clear").addEventListener("click", clearAll);
+
 function clearAll() {
   lastResult.textContent = "0";
   document.querySelector("#result1").textContent = "0";
   document.querySelector("#result2").textContent = "0";
   document.querySelector("#result3").textContent = "0";
+  document.querySelector("#doround").checked = false;
+  document.querySelector("#firstnumber").value = "";
+  document.querySelector("#secondnumber").value = "";
+  document.querySelector("#decimals").value = "";
   console.log("clearAll");
 }
